01f527dfd19ef8abfd6c2351fadcde24a60892dc
Anak Mirasing <thaihooligan@hotmail.com>
2015-11-07 21:01:42 +0700

    Try to use signal with UITextField

--- ReactiveCocoaSwift/ViewController.swift
+++ ReactiveCocoaSwift/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let searchTextSignal = searchTextField.rac_textSignal()
+        searchTextSignal.toSignalProducer().map({ text in text as! String }).startWithNext { text in
+            print("search text : \(text)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
